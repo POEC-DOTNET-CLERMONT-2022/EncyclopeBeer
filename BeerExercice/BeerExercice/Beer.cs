2d044d75da0917e03fc03998786d81ceb7851abc
Clément <clement.exe@gmail.com>
2021-12-22 15:28:38 +0100

    Création objet 'Beer' et classes liées

---
+++ BeerExercice/BeerExercice/Beer.cs
@@ -0,0 +1,38 @@
+﻿namespace BeerExercice
+{
+    internal class Beer
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public float Ibu { get; private set; }
+        public float Degree { get; private set; }
+        public string Flavour { get; private set; }
+        public string Taste { get; private set; }
+        public string Appearance { get; private set; }
+        public string Brewery { get; private set; }
+
+        private List<Ingredient> Ingredients = new List<Ingredient>();
+
+        private List<BeerStyle> BeerStyles = new List<BeerStyle>();
+
+        private List<BeerColor> BeerColors = new List<BeerColor>();
+
+        public Beer(int id, string name, float ibu, float degree, string flavour, string taste, string appearance, string brewery)
+        {
+            Id = id;
+            Name = name;
+            Ibu = ibu;  
+            Degree = degree;
+            Flavour = flavour;
+            Taste = taste;
+            Appearance = appearance;
+            Brewery = brewery;
+        }
+
+        public override string? ToString()
+        {
+            return $" - Nom: {Name}\n - IBU: {Ibu}\n - Degrés: {Degree}%\n - Arôme: {Flavour}\n - Gout: {Taste}\n - Apparence: {Appearance}\n - Brasserie : {Brewery}";
+        }
+
+    }
+}
---
+++ BeerExercice/BeerExercice/Ingredient/Ingredient.cs
@@ -0,0 +1,13 @@
+﻿namespace BeerExercice
+{
+    internal class Ingredient
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Ingredient()
+        {
+
+        }
+    }
+}
