e79cb14ab36af0408cb2948a6eb2d1ebf2697224
docapc <armel.pitelet@gmail.com>
2022-01-05 20:16:36 +0100

    Ajout Classe Additive et Cereal à l'ensemble Ingredient

---
+++ WikiBeer/Model/Ingredients/Additive.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ipme.WikiBeer.Model.Ingredients
+{
+    internal class Additive : Ingredient
+    {
+        public string Use { get; internal set; }
+
+        public Additive(string name, string use) : base(name)
+        {
+            Use = use;
+        }
+    }
+}
