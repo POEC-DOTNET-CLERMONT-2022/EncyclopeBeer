a0a6ebcc3f2f1a4cef03fa2ce2c12e1fc1e2afe2
docapc <armel.pitelet@gmail.com>
2022-01-09 18:38:23 +0100

    Ajout des méthodes AddBeer et DeleteBeer au BeerManager

--- WikiBeer/Persistance/BeerManager.cs
+++ WikiBeer/Persistance/BeerManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using AutoFixture;
 using Ipme.WikiBeer.Model;
 
@@ -15,6 +16,17 @@ namespace Ipme.WikiBeer.Persistance
             //Beers = new List<Beer>();
             _beers = new List<Beer>();
             _beers.AddRange(_fixture.CreateMany<Beer>(FixtureDefaultMagic.DEFAULT_BEER_NUMBER));
+        }
+
+        public void AddBeer(Beer beer_to_add)
+        {
+            _beers.Add(beer_to_add);
+        }
+
+        public void DeleteBeer(Beer beer_do_delete)
+        {
+            if (!_beers.Any())
+                _beers.Remove(beer_do_delete);
         }
 
         public IEnumerable<Beer> GetAllBeer()
