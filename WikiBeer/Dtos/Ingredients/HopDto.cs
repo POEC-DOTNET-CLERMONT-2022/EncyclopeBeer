67f88ecd4a68e51064aeea416ad03b93e64a1994
docapc <armel.pitelet@gmail.com>
2022-01-12 10:17:13 +0100

    modif alpha acid

--- WikiBeer/Dtos/Ingredients/HopDto.cs
+++ WikiBeer/Dtos/Ingredients/HopDto.cs
@@ -7,5 +7,10 @@ namespace Ipme.WikiBeer.Dtos.Ingredients
     internal class HopDto
     {
         public float AlphaAcid { get; internal set; }
+
+        public HopDto(float alpha_acid, string ttttt)
+        {
+            AlphaAcid = alpha_acid;
+        }
     }
 }
