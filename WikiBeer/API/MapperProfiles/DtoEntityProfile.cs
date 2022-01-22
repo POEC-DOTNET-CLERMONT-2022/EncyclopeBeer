acbf136fab8c5a9ac2136d46495914414416ddd9
docapc <armel.pitelet@gmail.com>
2022-01-22 13:58:28 +0100

    Préparation DbContext, Repos et Entities

---
+++ WikiBeer/API/MapperProfiles/DtoEntityProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+
+namespace Ipme.WikiBeer.API.MapperProfiles
+{
+    public class DtoEntityProfile : Profile
+    {
+        public DtoEntityProfile()
+        {
+            CreateMap<BeerDto, BeerEntity>().ReverseMap();
+
+            /// Exemples implicites : 
+            #region Types simples (Brassseur, Style, Couleur, Pays)
+            CreateMap<BreweryDto, BreweryEntity>().ReverseMap();
+            CreateMap<BeerColorDto, BeerColorEntity>().ReverseMap();
+            CreateMap<BeerStyleDto, BeerStyleEntity>().ReverseMap();
+            CreateMap<CountryDto, CountryEntity>().ReverseMap();
+            #endregion
+
+            #region Ingredients : abstract puis dérivées
+            /// voir https://docs.automapper.org/en/stable/Lists-and-arrays.html#
+            CreateMap<IngredientDto, IngredientEntity>().IncludeAllDerived();//.ReverseMap(); // IncludeAllDerived() pour inclure les types dérivées
+            CreateMap<IngredientEntity, IngredientDto>().IncludeAllDerived();
+            #region Dérivées
+            CreateMap<HopDto, HopEntity>().ReverseMap();
+            CreateMap<CerealDto, CerealEntity>().ReverseMap();
+            CreateMap<AdditiveDto, AdditiveEntity>().ReverseMap();
+            #endregion
+            #endregion
+
+        }
+    }
+}
