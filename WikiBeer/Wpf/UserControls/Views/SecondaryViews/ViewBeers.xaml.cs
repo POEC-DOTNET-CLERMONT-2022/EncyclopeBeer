46c078fae7934bc827e8c34411c4bc82ed90d5d3
Clément <clem.heritier@gmail.com>
2022-01-31 23:05:49 +0100

    fix/BindingComponent

--- WikiBeer/Wpf/UserControls/Views/SecondaryViews/ViewBeers.xaml.cs
+++ WikiBeer/Wpf/UserControls/Views/SecondaryViews/ViewBeers.xaml.cs
@@ -1,22 +1,10 @@
 ﻿using Ipme.WikiBeer.ApiDatas;
 using Ipme.WikiBeer.Dtos;
 using Ipme.WikiBeer.Models;
-using Ipme.WikiBeer.Wpf.UserControls.Components;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Ipme.WikiBeer.Wpf.UserControls.Views.SecondaryViews
 {
@@ -27,19 +15,20 @@ namespace Ipme.WikiBeer.Wpf.UserControls.Views.SecondaryViews
     {
         private IDataManager<BeerModel, BeerDto> _beerDataManager = ((App)Application.Current).BeerDataManager;
 
-        private BeersListModel BeersList { get; set; } = new BeersListModel();
+        public BeersListModel BeersList { get; set; } 
 
         public ViewBeers()
         {
+            BeersList = new BeersListModel();
             InitializeComponent();
         }
 
-        private async void Windows_Loaded(object sender, RoutedEventArgs e)
+        public async void Windows_Loaded(object sender, RoutedEventArgs e)
         {
             LoadBeers();
         }
 
-        public async void LoadBeers()
+        public async Task LoadBeers()
         {
             var beerModels = await _beerDataManager.GetAll();
             BeersList.Beers = new ObservableCollection<BeerModel>(beerModels);
