9c7be5e2c8440957f129a4a0a7b2088fa2eb2598
docapc <armel.pitelet@gmail.com>
2022-02-01 17:07:36 +0100

    Modif fin de journée, commentaires divers

--- WikiBeer/Entities/BeerStyleEntity.cs
+++ WikiBeer/Entities/BeerStyleEntity.cs
@@ -6,9 +6,9 @@
 
         public string Name { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
-        private BeerStyleEntity()
+        public BeerStyleEntity()
         {
         }
 
