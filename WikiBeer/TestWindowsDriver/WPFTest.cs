a3c551278eb6d711e82ee7a0a8d2efdeed4fbd4e
docapc <armel.pitelet@gmail.com>
2022-02-03 16:12:09 +0100

    Ajout Test WPf et Angular. Retrait de la partie Angular du sln

---
+++ WikiBeer/TestWindowsDriver/WPFTest.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting; // pour [ClassInitialize]
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Doit hériter de TestSession
+/// </summary>
+namespace TestWindowsDriver
+{
+    [TestClass]
+    internal class WPFTest : TestSession
+    {
+        /// <summary>
+        /// A l'ouverture de l'appli
+        /// </summary>
+        /// <param name="context"></param>
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
+            Setup(context);
+        }
+
+        [TestMethod]
+        public void TestApp()
+        {
+            //session.FindElementByName("UnNom").Click; // Attention c'est une recherche par Content
+            session.FindElementByAccessibilityId("LoginButton").Click(); // ici c'est le x:Name présent dans du xaml
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            
+        }
+
+        /// <summary>
+        /// A la fermeture de l'appli
+        /// </summary>
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            //TearDown()
+        }
+
+    }
+}
