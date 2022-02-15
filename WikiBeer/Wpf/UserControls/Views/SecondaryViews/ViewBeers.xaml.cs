572de188aac6dec4cf2f580a86c89384cf8d183b
Clément <clem.heritier@gmail.com>
2022-02-15 07:43:08 +0100

    transfert

--- WikiBeer/Wpf/UserControls/Views/SecondaryViews/ViewBeers.xaml.cs
+++ WikiBeer/Wpf/UserControls/Views/SecondaryViews/ViewBeers.xaml.cs
@@ -33,6 +33,7 @@ namespace Ipme.WikiBeer.Wpf.UserControls.Views.SecondaryViews
             Breweries = new GenericListModel<BreweryModel>();
             Styles = new GenericListModel<BeerStyleModel>();
             Colors = new GenericListModel<BeerColorModel>();
+            Ingredients = new GenericListModel<IngredientModel>();
             InitializeComponent();
         }
 
@@ -81,13 +82,16 @@ namespace Ipme.WikiBeer.Wpf.UserControls.Views.SecondaryViews
             var description = Beers.ToModify.Description;
             float ibu = Beers.ToModify.Ibu;
             float abv = Beers.ToModify.Degree;
-            BreweryModel brewery = (BreweryModel)BreweryDetailsComponent.BreweriesComboBox.SelectedItem;
-            BeerStyleModel style = (BeerStyleModel)BreweryDetailsComponent.StylesComboBox.SelectedItem;
-            BeerColorModel color = (BeerColorModel)BreweryDetailsComponent.ColorsComboBox.SelectedItem;
-            //var beer = new BeerModel(name, description, ibu, abv, style, color, brewery, );
-            //var newBeer = await _beerDataManager.Add(beer);
-            //Beers.List.Add(newBeer);
-            //Beers.ToModify = null;
+            BreweryModel brewery = (BreweryModel)BeerDetailsComponent.BreweriesComboBox.SelectedItem;
+            BeerStyleModel style = (BeerStyleModel)BeerDetailsComponent.StylesComboBox.SelectedItem;
+            BeerColorModel color = (BeerColorModel)BeerDetailsComponent.ColorsComboBox.SelectedItem;
+            IngredientModel ingredient = (IngredientModel)BeerDetailsComponent.IngredientsComboBox.SelectedItem;
+            ObservableCollection<IngredientModel> ingredients = new ObservableCollection<IngredientModel>();
+            ingredients.Add(ingredient);
+            var beer = new BeerModel(name, description, ibu, abv, style, color, brewery, ingredients);
+            var newBeer = await _beerDataManager.Add(beer);
+            Beers.List.Add(newBeer);
+            Beers.ToModify = null;
 
             Update_Button.Visibility = Visibility.Visible;
             Delete_Button.Visibility = Visibility.Visible;
@@ -95,14 +99,24 @@ namespace Ipme.WikiBeer.Wpf.UserControls.Views.SecondaryViews
             ListOverlay.Visibility = Visibility.Collapsed;
         }
 
-        private void Update_Button_Click(object sender, RoutedEventArgs e)
+        private async void Update_Button_Click(object sender, RoutedEventArgs e)
         {
-
+            //if (Beers.ToModify != null)
+            //{
+            //    await _beerDataManager.Update(Beers.ToModify.Id, Beers.ToModify);
+            //    var index = Beers.List.IndexOf(Beers.Current);
+            //    Beers.List[index] = Beers.ToModify.DeepClone();
+            //}
         }
 
-        private void Delete_Button_Click(object sender, RoutedEventArgs e)
+        private async void Delete_Button_Click(object sender, RoutedEventArgs e)
         {
-
+            if (Beers.ToModify != null)
+            {
+                await _beerDataManager.DeleteById(Beers.ToModify.Id);
+                Beers.List.Remove(Beers.Current);
+                Beers.ToModify = null;
+            }
         }
 
         private void Edit_Button_Click(object sender, RoutedEventArgs e)
