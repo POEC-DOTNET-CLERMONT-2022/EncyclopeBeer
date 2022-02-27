c4d1c65447558f2f6812fc4585d9dc9b898481fa
docapc <armel.pitelet@gmail.com>
2022-02-27 17:07:48 +0100

    Ajout check favoris dans card beer

--- WikiBeer/Web/src/app/components/beer-card/beer-card.component.ts
+++ WikiBeer/Web/src/app/components/beer-card/beer-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Beer } from 'src/models/beer';
+import { User } from 'src/models/users/user';
+import { UserService } from 'src/services/user.service';
 
 @Component({
   selector: 'app-beer-card',
@@ -8,13 +10,31 @@ import { Beer } from 'src/models/beer';
 })
 export class BeerCardComponent implements OnInit {
 
-  @Input() beer: Beer;
+  private _userService: UserService;
 
-  constructor()
+  @Input() beer: Beer;
+  public user: User;
+
+  constructor(userService: UserService)
   {
+    this._userService = userService
   }
 
   ngOnInit(): void {
+    this._userService.user.subscribe((u :User) => this.user = u);
+  }
+
+
+  isFavoriteBeer() : boolean  {
+
+    console.log(this.user.profile)
+    console.log(this.beer)
+    if(this.user.profile.favoriteBeerIds.includes(this.beer.id)){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 
 }
