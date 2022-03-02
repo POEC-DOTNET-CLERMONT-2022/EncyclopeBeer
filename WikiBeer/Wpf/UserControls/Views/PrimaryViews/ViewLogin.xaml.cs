33e6178590e425d7e36cb37e3fa5059bdbe8d2c7
docapc <armel.pitelet@gmail.com>
2022-03-02 23:05:34 +0100

    Ajout vérif certification avant authentification

--- WikiBeer/Wpf/UserControls/Views/PrimaryViews/ViewLogin.xaml.cs
+++ WikiBeer/Wpf/UserControls/Views/PrimaryViews/ViewLogin.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using Auth0.OidcClient;
+using Ipme.WikiBeer.ApiDatas;
 using Ipme.WikiBeer.Wpf.Utilities;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
+using System.Linq;
+using Ipme.WikiBeer.Models;
+using System.Threading.Tasks;
 
 namespace Ipme.WikiBeer.Wpf.UserControls.Views
 {
@@ -12,6 +16,7 @@ namespace Ipme.WikiBeer.Wpf.UserControls.Views
     public partial class ViewLogin : UserControl
     {
         public INavigator Navigator { get; set; } = ((App)Application.Current).Navigator;
+        private UserDataManager _userDataManager = (UserDataManager)((App)Application.Current).UserDataManager;
         private Auth0Client _client { get; set; }
 
 
@@ -25,14 +30,28 @@ namespace Ipme.WikiBeer.Wpf.UserControls.Views
         private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
             var loginResult = await AuthClient.LoginAsync();
-
+           
             if (loginResult.IsError)
             {
                 Debug.WriteLine($"An error occurred during login: {loginResult.Error}");
             }
             else
             {
+                var sub = loginResult.User.Claims.Where(c => c.Type == "sub").Select(sub => sub.Value).First();
+                var connectedUser = await _userDataManager.GetByConnectionId(sub);
+                await ContinueOrRetry(connectedUser);
+            }
+        }
+
+        private async Task ContinueOrRetry(UserModel user)
+        {
+            if (user.IsCertified)
+            {
                 Navigator.NavigateTo(typeof(ViewHome));
+            }
+            else
+            {
+                var logoutResult = await AuthClient.LogoutAsync();                
             }
         }
     }
