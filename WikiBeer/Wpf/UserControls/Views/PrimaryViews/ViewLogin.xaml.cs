ef5e7a1f64812e5037821ddb0f1b72225d14ede4
docapc <armel.pitelet@gmail.com>
2022-03-02 22:25:59 +0100

    réglage auth0

--- WikiBeer/Wpf/UserControls/Views/PrimaryViews/ViewLogin.xaml.cs
+++ WikiBeer/Wpf/UserControls/Views/PrimaryViews/ViewLogin.xaml.cs
@@ -25,6 +25,7 @@ namespace Ipme.WikiBeer.Wpf.UserControls.Views
         private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
             var loginResult = await AuthClient.LoginAsync();
+            //loginResult.IdentityToken.
 
             if (loginResult.IsError)
             {
